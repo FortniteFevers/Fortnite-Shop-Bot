edd6fcd63e6ba0c858df6cffc2b18f9e6fb03dcc
Fevers <47673608+FortniteFevers@users.noreply.github.com>
2022-01-15 19:36:15 -0500

    Update merger.py

--- merger.py
+++ merger.py
@@ -4,19 +4,20 @@ import requests
 import glob
 from math import ceil, sqrt
 from typing import Union
+import os
 
-response = requests.get('https://fortnite-api.com/v2/shop/br/combined')
-
-currentdate = response.json()['data']['date']
-currentdate = currentdate[:10]
 # Credits to https://github.com/MyNameIsDark01 for the original Merger code.
 # This merger is under rights, you may not take this code and use it in your own project without proper credits to Fevers and Dark.
 
 def merger(datas: Union[list, None] = None, save_as: str = f'shop.jpg'):
-    if not datas:
-        datas = [Image.open(i) for i in glob.glob('cache/*.png')]
+    
+    list_ = []
+    num = 0
+    for file in os.listdir('cache'):
+        num += 1
+        list_.append(f'cache/{file}')
 
-    row_n = len(datas)
+    row_n = num
         
     rowslen = ceil(sqrt(row_n))
     columnslen = round(sqrt(row_n))
@@ -29,6 +30,8 @@ def merger(datas: Union[list, None] = None, save_as: str = f'shop.jpg'):
     image = Image.new(mode, (rows, columns))
 
     i = 0
+
+    datas = [Image.open(i) for i in sorted(list_)]
 
     for card in datas:
         image.paste(
