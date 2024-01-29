656712a6d7a32e1e84060d0c597bcd603ea8c8a4
Fevers <47673608+FortniteFevers@users.noreply.github.com>
2024-01-29 17:32:02 -0500

    Final update

--- merger.py
+++ merger.py
@@ -13,22 +13,31 @@ currentdate = currentdate[:10]
 # Credits to https://github.com/MyNameIsDark01 for the original Merger code.
 # This merger is under rights, you may not take this code and use it in your own project without proper credits to Fevers and Dark.
 
-def merger(datas: Union[list, None] = None, save_as: str = f'shop {currentdate}.jpg'):
+def merger(ogitems, datas: Union[list, None] = None, save_as: str = f'shop {currentdate}.jpg'):
+    print(ogitems)
     response = requests.get('https://fortnite-api.com/v2/shop/br/combined')
     currentdate = response.json()['data']['date']
     currentdate = currentdate[:10]
     if not datas:
-        datas = [Image.open(i) for i in glob.glob('cache/*.png')]
+        if ogitems == False:
+            datas = [Image.open(i) for i in glob.glob('cache/*.png')]
+        else:
+            datas = [Image.open(i) for i in glob.glob('ogcache/*.png')]
 
     list_ = []
     num = 0
-    for file in os.listdir('cache'):
-        num += 1
-        
-        if file.startswith('tempzzz'):
-            pass
-        else:
-            list_.append(f'cache/{file}')
+    if ogitems == False:
+        for file in os.listdir('cache'):
+            num += 1
+            
+            if file.startswith('tempzzz'):
+                pass
+            else:
+                list_.append(f'cache/{file}')
+    else:
+        for file in os.listdir('ogcache'):
+            num += 1   
+            list_.append(f'ogcache/{file}')
 
     row_n = num
         
@@ -55,24 +64,30 @@ def merger(datas: Union[list, None] = None, save_as: str = f'shop {currentdate}.
 
         i += 1
 
-    image.save(f"{save_as}")
+    if ogitems == False:
+        image.save(f"{save_as}")
+    else:
+        image.save(f"OGitems.jpg")
 
-    img = PIL.Image.open(f"{save_as}")
-    width, height = img.size
+    #==== GENERATES TITLE ====#
 
-    img=Image.new("RGB",(width,height+322), 0x000000)
+    if ogitems == False:
+        img = PIL.Image.open(f"{save_as}")
+        width, height = img.size
 
-    shopimage = Image.open(f"{save_as}")
-    img.paste(shopimage, (0, 322))
-    
-    font=ImageFont.truetype('BurbankBigRegular-BlackItalic.otf',150)
-    draw=ImageDraw.Draw(img)
-    draw.text((width/2,190),'FORTNITE ITEM SHOP',font=font,fill='white', anchor='ms') # Writes name
+        img=Image.new("RGB",(width,height+322), 0x000000)
 
-    font=ImageFont.truetype('BurbankBigRegular-BlackItalic.otf',50)
-    draw.text((width/2,240),currentdate,font=font,fill='white', anchor='ms') # Writes name
+        shopimage = Image.open(f"{save_as}")
+        img.paste(shopimage, (0, 322))
+        
+        font=ImageFont.truetype('BurbankBigRegular-BlackItalic.otf',150)
+        draw=ImageDraw.Draw(img)
+        draw.text((width/2,190),'FORTNITE ITEM SHOP',font=font,fill='white', anchor='ms') # Writes name
 
-    img.save(f'{save_as}')
-    img.save('shop.jpg')
+        font=ImageFont.truetype('BurbankBigRegular-BlackItalic.otf',50)
+        draw.text((width/2,240),currentdate,font=font,fill='white', anchor='ms') # Writes name
 
-    return image
+        img.save(f'{save_as}')
+        img.save('shop.jpg')
+
+        return image
