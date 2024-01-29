3b3d7c4a1e5fd64c6432a5702f464a9f024b55d1
Fevers <47673608+FortniteFevers@users.noreply.github.com>
2024-01-28 21:36:21 -0500

    Update merger.py

--- merger.py
+++ merger.py
@@ -1,11 +1,12 @@
+import requests
 from PIL import Image, ImageFont, ImageDraw
 import PIL
-import requests
-import glob
+from datetime import datetime
+import os
+from colorama import *
 from math import ceil, sqrt
 from typing import Union
-import os
-
+import glob
 response = requests.get('https://fortnite-api.com/v2/shop/br/combined')
 
 currentdate = response.json()['data']['date']
@@ -13,7 +14,10 @@ currentdate = currentdate[:10]
 # Credits to https://github.com/MyNameIsDark01 for the original Merger code.
 # This merger is under rights, you may not take this code and use it in your own project without proper credits to Fevers and Dark.
 
-def merger(currentdate, datas: Union[list, None] = None, save_as: str = f'shop.jpg'):
+def merger(datas: Union[list, None] = None, save_as: str = f'shop {currentdate}.jpg'):
+    response = requests.get('https://fortnite-api.com/v2/shop/br/combined')
+    currentdate = response.json()['data']['date']
+    currentdate = currentdate[:10]
     if not datas:
         datas = [Image.open(i) for i in glob.glob('cache/*.png')]
 
@@ -57,7 +61,7 @@ def merger(currentdate, datas: Union[list, None] = None, save_as: str = f'shop.j
     img = PIL.Image.open(f"{save_as}")
     width, height = img.size
 
-    img=Image.new("RGB",(width,height+322))
+    img=Image.new("RGB",(width,height+322), 0x000000)
 
     shopimage = Image.open(f"{save_as}")
     img.paste(shopimage, (0, 322))
@@ -70,6 +74,6 @@ def merger(currentdate, datas: Union[list, None] = None, save_as: str = f'shop.j
     draw.text((width/2,240),currentdate,font=font,fill='white', anchor='ms') # Writes name
 
     img.save(f'{save_as}')
-    img.save(f'past_shops/{currentdate}.jpg')
+    img.save('shop.jpg')
 
     return image
