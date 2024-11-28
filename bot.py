8f518ac13741a1e3e30d79c6e2d648b8ce71906a
Fevers <47673608+FortniteFevers@users.noreply.github.com>
2024-11-27 19:13:46 -0500

    Update bot.py

--- bot.py
+++ bot.py
@@ -7,31 +7,7 @@ import time
 import shutil
 import tweepy
 
-#===============#
-loadFont = 'BurbankBigRegular-BlackItalic.otf' # File locaton the bot uses
-showItems = False # Lets you know every time the program generates a cosmetic (used for debugging)
-botDelay = 5 # Seconds the bot takes to update in updateMode
-
-ToggleTweet = False # True means the program uses your Twitter API keys. False means it does not.
-
-twitAPIKey = '' # DO NOT SHOW THIS KEY TO ANYONE
-twitAPISecretKey = '' # DO NOT SHOW THIS KEY TO ANYONE
-twitAccessToken = ''  # DO NOT SHOW THIS KEY TO ANYONE
-twitAccessTokenSecret = '' # DO NOT SHOW THIS KEY TO ANYONE
-
-# CHANGE UPDATE MODE TO FALSE IF "ToggleTweet" IS FALSE!!!
-updateMode = False # False means it instantly tweets it, True means it keeps refreshing until shop updates
-
-showData = True # Only used when ToggleTweet is "True", posts a tweet with extra shop information
-
-CreatorCode = 'Fevers' # Used for posting on Twitter :)
-
-OGitemsbot = True
-opitemdate = 180 # Threshold for classifying "Rare" items
-
-archiveShop = True # If True, the program saves a copy of the Item Shop with the corresponding date
-#===============#
-
+from config import *
 
 if ToggleTweet == True:
     print("\n! ! ! TWEETING IS ON ! ! !\n")
