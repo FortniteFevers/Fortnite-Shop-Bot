a8805171ac025e91d3b3b62de1b2b95e3e21738f
Fevers <47673608+FortniteFevers@users.noreply.github.com>
2024-11-28 10:04:42 -0500

    VERSION 1.7

---
+++ config.py
@@ -0,0 +1,27 @@
+# Configuration for Fortnite-Shop-Bot
+# Created by Fevers
+
+loadFont = 'BurbankBigRegular-BlackItalic.otf'  # File location the bot uses
+debugItems = False  # Lets you know every time the program generates a cosmetic (used for debugging)
+botDelay = 5  # Seconds the bot takes to update in updateMode
+
+sideItemLoader = False # Toggles generating side items in the shop! Works with bundles and singular items. (Note: This increases loading time significantly)
+
+ToggleTweet = False  # True means the program uses your Twitter API keys. False means it does not.
+
+twitAPIKey = ''  # DO NOT SHOW THIS KEY TO ANYONE
+twitAPISecretKey = ''  # DO NOT SHOW THIS KEY TO ANYONE
+twitAccessToken = ''  # DO NOT SHOW THIS KEY TO ANYONE
+twitAccessTokenSecret = ''  # DO NOT SHOW THIS KEY TO ANYONE
+
+# CHANGE UPDATE MODE TO FALSE IF "ToggleTweet" IS FALSE!!!
+updateMode = False  # False means it instantly tweets it, True means it keeps refreshing until shop updates
+
+showData = True  # Only used when ToggleTweet is "True", posts a tweet with extra shop information
+
+CreatorCode = 'Fevers'  # Used for posting on Twitter :)
+
+OGitemsbot = True
+opitemdate = 180  # Threshold for classifying "Rare" items
+
+archiveShop = True  # If True, the program saves a copy of the Item Shop with the corresponding date
